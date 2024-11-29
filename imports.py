8498f255b557b547c2f9f0d131115081490daa3a
Luigi Tomassone <Luigitomassone@gmail.com>
2024-11-29 08:26:52 -0300

    init html

--- imports.py
+++ imports.py
@@ -1,6 +1,7 @@
 from routes.main_routes import main_routes
 from routes.post_routes import post_routes
 
+
 def register_blueprints(app):
     app.register_blueprint(main_routes)
     app.register_blueprint(post_routes)
--- routes/main_routes.py
+++ routes/main_routes.py
@@ -5,11 +5,20 @@ main_routes = Blueprint('main_routes', __name__)
 
 
 @main_routes.route('/')
-def index():
-    leituras = Leitura.query.order_by(Leitura.data.desc()).all()
-    return render_template('index/index.html', title='Página Inicial', leituras=leituras)
-
+def home():
+    return render_template('home/home.html', title='Home')
 
 @main_routes.route('/sobre')
 def sobre():
     return render_template('index/sobre.html', title='Sobre o Projeto')
+
+
+@main_routes.route('/dados')
+def index():
+    # leituras = Leitura.query.order_by(Leitura.data.desc()).all()
+    return render_template('arduino/arduino.html', title='Dados')
+
+
+@main_routes.route('/perfil')
+def perfil():
+    return render_template('perfil/perfil.html', title='Perfil')
