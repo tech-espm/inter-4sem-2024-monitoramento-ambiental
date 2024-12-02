b00fc9666cf6a1abeead5101dc5b5e79d18adb9f
Luigi Tomassone <Luigitomassone@gmail.com>
2024-12-02 07:39:42 -0300

    html update

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -1,24 +1,72 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, json
 from models.leitura import Leitura
 
 main_routes = Blueprint('main_routes', __name__)
 
-
 @main_routes.route('/')
-def home():
-    return render_template('home/home.html', title='Home')
+def index():
+    return render_template('index/index.html', title='Home')
 
 @main_routes.route('/sobre')
 def sobre():
     return render_template('index/sobre.html', title='Sobre o Projeto')
 
+@main_routes.route('/dadosPorHora')
+def dadosPorHora():
+    return render_template('index/dadosPorHora.html', title='Dados por Hora')
 
-@main_routes.route('/dados')
-def index():
+@main_routes.route('/dadosPorDia')
+def dadosPorDia():
+    return render_template('index/dadosPorDia.html', title='Dados por Dia')
+
+@main_routes.route('/dadosPorHoraJson')
+def dadosPorHoraJson():
+    print(request.args.get('data'))
+
     # leituras = Leitura.query.order_by(Leitura.data.desc()).all()
-    return render_template('arduino/arduino.html', title='Dados')
+    leituras = [
+        {
+            'hora': '10:00',
+            'direcao_vento': 80,
+            'umidade_ar': 40,
+            'temperatura': 35,
+            'luminosidade': 50
+		},
+        {
+            'hora': '11:00',
+            'direcao_vento': 85,
+            'umidade_ar': 45,
+            'temperatura': 40,
+            'luminosidade': 55
+		},
+	]
 
+    return json.jsonify(leituras)
+
+@main_routes.route('/dadosPorDiaJson')
+def dadosPorDiaJson():
+    print(request.args.get('mes'))
+
+    # leituras = Leitura.query.order_by(Leitura.data.desc()).all()
+    leituras = [
+        {
+            'dia': '01/11',
+            'direcao_vento': 80,
+            'umidade_ar': 40,
+            'temperatura': 35,
+            'luminosidade': 50
+		},
+        {
+            'dia': '02/11',
+            'direcao_vento': 85,
+            'umidade_ar': 45,
+            'temperatura': 40,
+            'luminosidade': 55
+		},
+	]
+
+    return json.jsonify(leituras)
 
 @main_routes.route('/perfil')
 def perfil():
-    return render_template('perfil/perfil.html', title='Perfil')
+    return render_template('index/perfil.html', title='Perfil')
--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,5 +1,6 @@
 from models.leitura import db
 
 def init_db(app):
-    with app.app_context():
-        db.create_all()
+    #with app.app_context():
+    #    db.create_all()
+	print('db')
